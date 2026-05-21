8b8e167296ebec59e714373a001b93157124f5d7
TaiChi <anothai.0978452316@gmail.com>
2026-05-21 21:47:21 +0700

    chore: remove temporary redirect from page component

--- apps/web/src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function Page() {
-  // Temporary server-side redirect so the prototype v5 page becomes the site root
-  redirect("/prototype/v5");
-}
